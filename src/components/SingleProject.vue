66dd5e92b1c30c4cec435c295520f923b865116d
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2023-08-01 22:52:17 -0500

    Created a component for updating details in a project. Practice submitting PATCH requests to JSON-SERVER.

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,84 +1,87 @@
 <template>
   <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
-        <h3 @click="this.showDetails = !this.showDetails">{{ project.title }}</h3>
-        <div class="icons">
-            <span class="material-icons">edit</span>
-            <span @click="deleteProject" class="material-icons">delete</span>
-            <span @click="toggleComplete" class="material-icons tick">done</span>
-        </div>
-
+      <h3 @click="this.showDetails = !this.showDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <router-link :to="{ name: 'EditProject', params: { id: project.id }}">
+          <span class="material-icons">edit</span>
+        </router-link>
+        <span @click="deleteProject" class="material-icons">delete</span>
+        <span @click="toggleComplete" class="material-icons tick">done</span>
+      </div>
     </div>
     <div class="details" v-if="showDetails">
-        <p> {{ project.details }} </p>
+      <p>{{ project.details }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['project'],
-    data() {
-        return {
-            showDetails: false,
-            uri: 'http://localhost:3000/projects/' + this.project.id
-        }
+  props: ["project"],
+  data() {
+    return {
+      showDetails: false,
+      uri: "http://localhost:3000/projects/" + this.project.id,
+    };
+  },
+  methods: {
+    deleteProject() {
+      fetch(this.uri, { method: "DELETE" })
+        .then(() => this.$emit("deleted", this.project.id))
+        .catch((err) => console.log(err));
     },
-    methods: {
-        deleteProject() {
-            fetch(this.uri, {method: 'DELETE'})
-                .then(() => this.$emit('deleted', this.project.id))
-                .catch(err => console.log(err))
-        },
-        toggleComplete() {
-            fetch(this.uri, { 
-                method: 'PATCH',            //specify what action you are performing
-                headers: { 'Content-Type': 'application/json' },    
-                body: JSON.stringify({ complete: !this.project.complete})       //cant just send js objects as is, need to convert to a JSON string first
-            }).then(() => {
-                this.$emit('complete', this.project.id)
-            }).catch((err) =>console.log(err))
-        }
-    }
-}
+    toggleComplete() {
+      fetch(this.uri, {
+        method: "PATCH", //specify what action you are performing
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ complete: !this.project.complete }), //cant just send js objects as is, need to convert to a JSON string first
+      })
+        .then(() => {
+          this.$emit("complete", this.project.id);
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+};
 </script>
 
 <style>
 .project {
-    margin: 20px auto;
-    background: white;
-    padding: 10px 20px;
-    border-radius: 4px;
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
-    border-left: 4px solid #e90074;
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90074;
 }
 
 h3 {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .actions {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .material-icons {
-    font-size: 24px;
-    margin-left: 10px;
-    color: #bbb;
-    cursor: pointer;
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
 }
 
 .material-icons:hover {
-    color: #777;
+  color: #777;
 }
 
 .project.complete {
-    border-left: 4px solid #00ce89;
+  border-left: 4px solid #00ce89;
 }
 
 .project.complete .tick {
-    color:#00ce89;
+  color: #00ce89;
 }
 </style>
